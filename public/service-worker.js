33a2923f2a317719c9d5473f231e382a9aac9555
VOK1SHARP <102311797+VOK1SHARP@users.noreply.github.com>
2025-12-02 03:16:48 +0300

    Update service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -5,47 +5,23 @@ const urlsToCache = [
     '/index.html',
     '/style.css',
     '/app.js',
-    '/manifest.json',
-    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
-    'https://telegram.org/js/telegram-web-app.js'
+    '/manifest.json'
 ];
 
-// Установка Service Worker
 self.addEventListener('install', event => {
     event.waitUntil(
         caches.open(CACHE_NAME)
             .then(cache => {
-                console.log('Opened cache');
+                console.log('Cache opened');
                 return cache.addAll(urlsToCache);
             })
-            .catch(error => {
-                console.error('Cache installation failed:', error);
-            })
     );
 });
 
-// Активация Service Worker
-self.addEventListener('activate', event => {
-    event.waitUntil(
-        caches.keys().then(cacheNames => {
-            return Promise.all(
-                cacheNames.map(cacheName => {
-                    if (cacheName !== CACHE_NAME) {
-                        console.log('Deleting old cache:', cacheName);
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
-        })
-    );
-});
-
-// Перехват запросов
 self.addEventListener('fetch', event => {
     event.respondWith(
         caches.match(event.request)
             .then(response => {
-                // Возвращаем кэшированный ресурс или делаем сетевой запрос
                 return response || fetch(event.request);
             })
     );
